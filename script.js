a7402742a2e184f32807558b0f553e35d86b13d6
APURVA122 <mj8893854@gmail.com>
2026-06-15 22:38:19 +0530

    add window 98 style interface

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
+// Clock
 
-function updateClock(){
+function updateClock() {
     document.getElementById("clock").innerText =
-    new Date().toLocaleTimeString([],{
-        hour:"2-digit",
-        minute:"2-digit"
-    });
+        new Date().toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit"
+        });
 }
 
 updateClock();
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
 
+// Windows
+
+const projectsWindow = document.getElementById("projectsWindow");
+const aboutWindow = document.getElementById("aboutWindow");
+
+// My Computer
+
+document.getElementById("computerIcon").addEventListener("click", () => {
+    location.reload();
+});
+
+// Projects
+
+document.getElementById("projectsIcon").addEventListener("click", () => {
+    projectsWindow.style.display = "block";
+});
+
+// About
+
+document.getElementById("aboutIcon").addEventListener("click", () => {
+    aboutWindow.style.display = "block";
+});
+
+// Close Buttons
+
+function closeProjects() {
+    projectsWindow.style.display = "none";
+}
+
+function closeAbout() {
+    aboutWindow.style.display = "none";
+}
